05790f746c41a51dc09e0c466505302c6d42f99b
TwmMeredydd <49553663+TwmMeredydd@users.noreply.github.com>
2023-05-24 23:54:42 +0100

    Initial creative tab implementation

--- fabric/src/main/java/twmmeredydd/atrusdoors/fabric/AtrusDoorsFabric.java
+++ fabric/src/main/java/twmmeredydd/atrusdoors/fabric/AtrusDoorsFabric.java
@@ -1,14 +1,29 @@
 package twmmeredydd.atrusdoors.fabric;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import twmmeredydd.atrusdoors.AtrusDoors;
 import twmmeredydd.atrusdoors.item.AtrusDoorsItems;
+import twmmeredydd.atrusdoors.item.BaseItem;
 
 import java.util.Map;
 
 public class AtrusDoorsFabric implements ModInitializer {
+    public static final CreativeModeTab TAB = FabricItemGroup.builder(AtrusDoors.id("main"))
+            .icon(() -> ItemStack.EMPTY)
+            .displayItems((itemDisplayParameters, output) -> {
+                AtrusDoorsItems.ITEMS.values().forEach(t -> {
+                    if (((BaseItem)t).isInGroup()) {
+                        output.accept(t);
+                    }
+                });
+            })
+            .build();
 
     @Override
     public void onInitialize() {
--- forge/src/main/java/twmmeredydd/atrusdoors/forge/AtrusDoorsForge.java
+++ forge/src/main/java/twmmeredydd/atrusdoors/forge/AtrusDoorsForge.java
@@ -2,14 +2,18 @@ package twmmeredydd.atrusdoors.forge;
 
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.RegisterEvent;
 import twmmeredydd.atrusdoors.AtrusDoors;
 import twmmeredydd.atrusdoors.item.AtrusDoorsItems;
+import twmmeredydd.atrusdoors.item.BaseItem;
 
 import java.util.Map;
 
@@ -21,9 +25,28 @@ public class AtrusDoorsForge {
         bus = FMLJavaModLoadingContext.get().getModEventBus();
 
         register(Registries.ITEM, AtrusDoorsItems.ITEMS);
+        registerItemGroup();
     }
 
     public <T> void register(ResourceKey<Registry<T>> registry, Map<ResourceLocation, T> map) {
         bus.addListener((RegisterEvent event) -> map.forEach((r, t) -> event.register(registry, r, () -> t)));
+    }
+
+    //Replace with custom class provider which other registries can easily be added to or something.
+
+    public void registerItemGroup() {
+        bus.addListener((CreativeModeTabEvent.Register event) -> {
+            event.registerCreativeModeTab(AtrusDoors.id("main"), builder -> {
+                builder.title(Component.translatable("itemGroup." + AtrusDoors.MOD_ID + ".main"));
+                builder.icon(() -> ItemStack.EMPTY);
+                builder.displayItems((itemDisplayParameters, output) -> {
+                    AtrusDoorsItems.ITEMS.values().forEach(t -> {
+                        if (((BaseItem)t).isInGroup()) {
+                            output.accept(t);
+                        }
+                    });
+                });
+            });
+        });
     }
 }
