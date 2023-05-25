0468eaa6121b5684d63c2e688437d68ea3690d88
TwmMeredydd <49553663+TwmMeredydd@users.noreply.github.com>
2023-05-25 20:37:13 +0100

    Create barebones LinkingBookItem

--- common/src/main/java/twmmeredydd/atrusdoors/item/AtrusDoorsItems.java
+++ common/src/main/java/twmmeredydd/atrusdoors/item/AtrusDoorsItems.java
@@ -3,6 +3,7 @@ package twmmeredydd.atrusdoors.item;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import twmmeredydd.atrusdoors.AtrusDoors;
+import twmmeredydd.atrusdoors.item.data.LinkingBookItem;
 
 import java.util.LinkedHashMap;
 import java.util.function.BiConsumer;
@@ -11,7 +12,8 @@ public class AtrusDoorsItems {
     public static final LinkedHashMap<ResourceLocation, Item> ITEMS = new LinkedHashMap<>();
 
     public static final Item LINKING_PANEL = create("linking_panel", new AtrusDoorsItem(new Item.Properties(), true));
-    public static final UnlinkedLinkingBookItem UNLINKED_LINKING_BOOK_ITEM = create("unlinked_linking_book", new UnlinkedLinkingBookItem(new Item.Properties(), true));
+    public static final UnlinkedLinkingBookItem UNLINKED_LINKING_BOOK = create("unlinked_linking_book", new UnlinkedLinkingBookItem(new Item.Properties(), true));
+    public static final LinkingBookItem LINKING_BOOK = create("linking_book", new LinkingBookItem(new Item.Properties().stacksTo(1), false));
 
     public static <T extends Item> T create(String name, T item) {
         if (ITEMS.put(AtrusDoors.id(name), item) != null) {
---
+++ common/src/main/java/twmmeredydd/atrusdoors/item/data/LinkingBookItem.java
@@ -0,0 +1,9 @@
+package twmmeredydd.atrusdoors.item.data;
+
+import twmmeredydd.atrusdoors.item.AtrusDoorsItem;
+
+public class LinkingBookItem extends AtrusDoorsItem {
+    public LinkingBookItem(Properties properties, boolean isInTab) {
+        super(properties, isInTab);
+    }
+}
