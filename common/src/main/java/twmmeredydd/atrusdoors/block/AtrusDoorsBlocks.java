8bbf0b300164bfcd328e56fa72fc2ce9239a570f
TwmMeredydd <49553663+TwmMeredydd@users.noreply.github.com>
2023-07-05 23:42:20 +0100

    Lock cherry and bamboo bookstand variants behind 1.20 experimental features.

--- common/src/main/java/twmmeredydd/atrusdoors/block/AtrusDoorsBlocks.java
+++ common/src/main/java/twmmeredydd/atrusdoors/block/AtrusDoorsBlocks.java
@@ -1,6 +1,7 @@
 package twmmeredydd.atrusdoors.block;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -17,17 +18,17 @@ import java.util.LinkedHashMap;
 public class AtrusDoorsBlocks {
     public static final LinkedHashMap<ResourceLocation, Block> BLOCKS = new LinkedHashMap<>();
 
-    public static final BookstandBlock OAK_BOOKSTAND = createBookstand(WoodType.OAK, Blocks.OAK_PLANKS);
-    public static final BookstandBlock SPRUCE_BOOKSTAND = createBookstand(WoodType.SPRUCE, Blocks.SPRUCE_PLANKS);
-    public static final BookstandBlock BIRCH_BOOKSTAND = createBookstand(WoodType.BIRCH, Blocks.BIRCH_PLANKS);
-    public static final BookstandBlock ACACIA_BOOKSTAND = createBookstand(WoodType.ACACIA, Blocks.ACACIA_PLANKS);
-    public static final BookstandBlock CHERRY_BOOKSTAND = createBookstand(WoodType.CHERRY, Blocks.CHERRY_PLANKS);
-    public static final BookstandBlock JUNGLE_BOOKSTAND = createBookstand(WoodType.JUNGLE, Blocks.JUNGLE_PLANKS);
-    public static final BookstandBlock DARK_OAK_BOOKSTAND = createBookstand(WoodType.DARK_OAK, Blocks.DARK_OAK_PLANKS);
-    public static final BookstandBlock CRIMSON_BOOKSTAND = createBookstand(WoodType.CRIMSON, Blocks.CRIMSON_PLANKS);
-    public static final BookstandBlock WARPED_BOOKSTAND = createBookstand(WoodType.WARPED, Blocks.WARPED_PLANKS);
-    public static final BookstandBlock MANGROVE_BOOKSTAND = createBookstand(WoodType.MANGROVE, Blocks.MANGROVE_PLANKS);
-    public static final BookstandBlock BAMBOO_BOOKSTAND = createBookstand(WoodType.BAMBOO, Blocks.BAMBOO);
+    public static final BookstandBlock OAK_BOOKSTAND = createBookstand(WoodType.OAK, Blocks.OAK_PLANKS, false);
+    public static final BookstandBlock SPRUCE_BOOKSTAND = createBookstand(WoodType.SPRUCE, Blocks.SPRUCE_PLANKS, false);
+    public static final BookstandBlock BIRCH_BOOKSTAND = createBookstand(WoodType.BIRCH, Blocks.BIRCH_PLANKS, false);
+    public static final BookstandBlock ACACIA_BOOKSTAND = createBookstand(WoodType.ACACIA, Blocks.ACACIA_PLANKS, false);
+    public static final BookstandBlock CHERRY_BOOKSTAND = createBookstand(WoodType.CHERRY, Blocks.CHERRY_PLANKS, true);
+    public static final BookstandBlock JUNGLE_BOOKSTAND = createBookstand(WoodType.JUNGLE, Blocks.JUNGLE_PLANKS, false);
+    public static final BookstandBlock DARK_OAK_BOOKSTAND = createBookstand(WoodType.DARK_OAK, Blocks.DARK_OAK_PLANKS, false);
+    public static final BookstandBlock CRIMSON_BOOKSTAND = createBookstand(WoodType.CRIMSON, Blocks.CRIMSON_PLANKS, false);
+    public static final BookstandBlock WARPED_BOOKSTAND = createBookstand(WoodType.WARPED, Blocks.WARPED_PLANKS, false);
+    public static final BookstandBlock MANGROVE_BOOKSTAND = createBookstand(WoodType.MANGROVE, Blocks.MANGROVE_PLANKS, false);
+    public static final BookstandBlock BAMBOO_BOOKSTAND = createBookstand(WoodType.BAMBOO, Blocks.BAMBOO, true);
 
     public static <T extends Block> T create(String name, T block, boolean createItem) {
         ResourceLocation id = AtrusDoors.id(name);
@@ -38,7 +39,9 @@ public class AtrusDoorsBlocks {
         return block;
     }
 
-    private static BookstandBlock createBookstand(WoodType type, Block planks) {
-        return create(type.name() + "_bookstand", new BookstandBlock(BlockBehaviour.Properties.of(Material.WOOD, planks.defaultMaterialColor()).strength(1).sound(SoundType.WOOD)), true);
+    private static BookstandBlock createBookstand(WoodType type, Block planks, boolean is120Exclusive) {
+        BlockBehaviour.Properties props = BlockBehaviour.Properties.of(Material.WOOD, planks.defaultMaterialColor()).strength(1).sound(SoundType.WOOD);
+        if (is120Exclusive) props.requiredFeatures(FeatureFlags.UPDATE_1_20);
+        return create(type.name() + "_bookstand", new BookstandBlock(props), true);
     }
 }
