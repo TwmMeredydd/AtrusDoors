e6b94955ed670466131cabac79293d99e68a31f7
TwmMeredydd <49553663+TwmMeredydd@users.noreply.github.com>
2023-07-05 16:14:14 +0100

    Configure fabric datagen, exporting to :common/src/main/generated

---
+++ fabric/src/main/java/twmmeredydd/atrusdoors/fabric/datagen/AtrusDoorsDatagen.java
@@ -0,0 +1,11 @@
+package twmmeredydd.atrusdoors.fabric.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AtrusDoorsDatagen implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
