ba8c7ef4f4db36874775287d6b001c02e8d29ae5
TwmMeredydd <49553663+TwmMeredydd@users.noreply.github.com>
2023-08-08 22:41:09 +0100

    Delete unneeded IRegisterInterface

--- common/src/main/java/twmmeredydd/atrusdoors/registry/IRegister.java
+++
@@ -1,8 +0,0 @@
-package twmmeredydd.atrusdoors.registry;
-
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceKey;
-
-public interface IRegister {
-    <T> IRegister create(ResourceKey<? extends Registry<T>> registryKey, String modid);
-}
