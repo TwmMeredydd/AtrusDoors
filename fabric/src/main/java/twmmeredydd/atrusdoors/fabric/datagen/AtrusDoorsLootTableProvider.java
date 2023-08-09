7db74e33dd6f16eaf96a7be1f35174a27c7ef1f8
TwmMeredydd <49553663+TwmMeredydd@users.noreply.github.com>
2023-08-09 09:09:44 +0100

    Fix block references in AtrusDoorsLootTableProvider

--- fabric/src/main/java/twmmeredydd/atrusdoors/fabric/datagen/AtrusDoorsLootTableProvider.java
+++ fabric/src/main/java/twmmeredydd/atrusdoors/fabric/datagen/AtrusDoorsLootTableProvider.java
@@ -11,15 +11,15 @@ public class AtrusDoorsLootTableProvider extends FabricBlockLootTableProvider {
 
     @Override
     public void generate() {
-        dropSelf(AtrusDoorsBlocks.OAK_BOOKSTAND);
-        dropSelf(AtrusDoorsBlocks.SPRUCE_BOOKSTAND);
-        dropSelf(AtrusDoorsBlocks.BIRCH_BOOKSTAND);
-        dropSelf(AtrusDoorsBlocks.ACACIA_BOOKSTAND);
-        dropSelf(AtrusDoorsBlocks.CHERRY_BOOKSTAND);
-        dropSelf(AtrusDoorsBlocks.JUNGLE_BOOKSTAND);
-        dropSelf(AtrusDoorsBlocks.DARK_OAK_BOOKSTAND);
-        dropSelf(AtrusDoorsBlocks.CRIMSON_BOOKSTAND);
-        dropSelf(AtrusDoorsBlocks.WARPED_BOOKSTAND);
-        dropSelf(AtrusDoorsBlocks.MANGROVE_BOOKSTAND);
+        dropSelf(AtrusDoorsBlocks.OAK_BOOKSTAND.get());
+        dropSelf(AtrusDoorsBlocks.SPRUCE_BOOKSTAND.get());
+        dropSelf(AtrusDoorsBlocks.BIRCH_BOOKSTAND.get());
+        dropSelf(AtrusDoorsBlocks.ACACIA_BOOKSTAND.get());
+        dropSelf(AtrusDoorsBlocks.CHERRY_BOOKSTAND.get());
+        dropSelf(AtrusDoorsBlocks.JUNGLE_BOOKSTAND.get());
+        dropSelf(AtrusDoorsBlocks.DARK_OAK_BOOKSTAND.get());
+        dropSelf(AtrusDoorsBlocks.CRIMSON_BOOKSTAND.get());
+        dropSelf(AtrusDoorsBlocks.WARPED_BOOKSTAND.get());
+        dropSelf(AtrusDoorsBlocks.MANGROVE_BOOKSTAND.get());
     }
 }
